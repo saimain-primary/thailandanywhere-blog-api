ab1de60913f7341d39551d947e3cd3736d3dc024
saimain-primary <saimain.primary@gmail.com>
2023-06-30 21:49:21 +0630

    category and post

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','https://sales-admin.thanywhere.com'],
+    'allowed_origins' => ['http://localhost:5173','https://sales-admin.thanywhere.com','https://kaungblogui.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
