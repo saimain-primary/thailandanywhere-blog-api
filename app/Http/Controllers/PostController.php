af70889bbbb26fae1d0ea791582e01694f57f869
saimain-primary <saimain.primary@gmail.com>
2023-07-01 10:48:25 +0630

    category and post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -82,7 +82,7 @@ class PostController extends Controller
                 ],
             ])
             ->response()
-            ->getData(), 'Popular Post List');
+            ->getData(), 'Feature Post List');
     }
 
     public function getRecentPost(Request $request)
