39971efbf90c736c8e8143bd8180da4872b64f37
saimain-primary <saimain.primary@gmail.com>
2023-07-04 14:29:51 +0630

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','https://sales-admin.thanywhere.com','https://blog.thanywhere.com'],
+    'allowed_origins' => ['https://sales-admin.thanywhere.com','https://blog.thanywhere.com'],
 
     'allowed_origins_patterns' => [],
 
