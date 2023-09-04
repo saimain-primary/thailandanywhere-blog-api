daaa8dd6416b0ae2073ed904137546147e46f708
Sai Main <saimain.primary@gmail.com>
2023-09-04 15:23:23 +0630

    pickup

--- resources/views/pdf/booking_receipt.blade.php
+++ resources/views/pdf/booking_receipt.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <style>
         @page {
             margin: 0px;
@@ -17,7 +19,7 @@
         }
 
         body {
-            font-family: sans-serif;
+            font-family: 'Poppins', sans-serif;
             margin: 0 !important;
             padding: 0 !important;
             width: 100%;
