e55aa701c97f6dced800420216c90e13fd048fce
Sai Main <saimain.primary@gmail.com>
2023-09-18 15:54:19 +0630

    awaiting

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -42,13 +42,13 @@ class ReservationController extends Controller
         $crmId = $request->query('crm_id');
 
         if ($productType) {
-            $query->whereHas('items', function ($q) use ($productType) {
+            $query->has('items', function ($q) use ($productType) {
                 $q->where('product_type', $productType);
             })->with(['items' => function ($q) use ($productType, $crmId) {
+                $q->where('product_type', $productType);
                 if($crmId) {
                     $q->where('crm_id', $crmId);
                 }
-                $q->where('product_type', $productType);
             }]);
         }
 
