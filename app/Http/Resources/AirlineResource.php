fbbd46141d0e794d2b23d2f768b75fef43c41a4d
Sai Main <saimain.primary@gmail.com>
2023-09-20 22:50:21 +0630

    attraction

--- app/Http/Resources/AirlineResource.php
+++ app/Http/Resources/AirlineResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AirlineResource extends JsonResource
@@ -14,6 +15,16 @@ class AirlineResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'legal_name' => $this->legal_name,
+            'starting_balance' => $this->starting_balance,
+            'contract' => $this->contract ? env('APP_URL', 'http://localhost:8000') . Storage::url('contracts/' . $this->contract) : null,
+            'deleted_at' => $this->deleted_at,
+            'updated_at' => $this->updated_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
