0ca7fe6d8dce74301b54d026ea40e297cddfa99b
Sai Main <saimain.primary@gmail.com>
2023-10-05 22:20:28 +0630

    receipt

--- resources/views/pdf/booking_receipt.blade.php
+++ resources/views/pdf/booking_receipt.blade.php
@@ -274,27 +274,27 @@
                                 {{ $data->payment_currency }}
                             </td>
                         </tr>
-                        <tr>
-                            <td></td>
-                            <td>PAYMENT STATUS</td>
-                            @if ($data->payment_status === 'not_paid')
-                                <td style="font-weight: bold; font-size:14px; color:red">
-                                    {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
-                                </td>
-                            @endif
-                            @if ($data->payment_status === 'partially_paid')
-                                <td style="font-weight: bold; font-size:14px; color:yellow">
-                                    {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
-                                </td>
-                            @endif
-                            @if ($data->payment_status === 'fully_paid')
-                                <td style="font-weight: bold; font-size:14px; color: green">
-                                    {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
-                                </td>
-                            @endif
 
-                        </tr>
                     @endif
+                    <tr>
+                        <td></td>
+                        <td>PAYMENT STATUS</td>
+                        @if ($data->payment_status === 'not_paid')
+                            <td style="font-weight: bold; font-size:14px; color:red">
+                                {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
+                            </td>
+                        @endif
+                        @if ($data->payment_status === 'partially_paid')
+                            <td style="font-weight: bold; font-size:14px; color:yellow">
+                                {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
+                            </td>
+                        @endif
+                        @if ($data->payment_status === 'fully_paid')
+                            <td style="font-weight: bold; font-size:14px; color: green">
+                                {{ ucwords(str_replace('_', ' ', $data->payment_status)) }}
+                            </td>
+                        @endif
+                    </tr>
                 </tbody>
             </table>
         </div>
